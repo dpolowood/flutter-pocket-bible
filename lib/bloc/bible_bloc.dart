97f861b314a81c8bbc1fddb1723a48f6e41c7be6
Daniel <danny.polowood@gmail.com>
2020-03-03 12:47:38 -0500

    deleted some version info

--- lib/bloc/bible_bloc.dart
+++ lib/bloc/bible_bloc.dart
@@ -127,8 +127,6 @@ class BibleBloc with ChangeNotifier {
 
     PackageInfo packageInfo = await PackageInfo.fromPlatform();
 
-    String appName = packageInfo.appName;
-    String packageName = packageInfo.packageName;
     String version = packageInfo.version;
     String buildNumber = packageInfo.buildNumber;
     
